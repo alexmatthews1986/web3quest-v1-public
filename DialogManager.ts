61b85c668b07807fc54d3239f3fc16a656d5e54a
alexmatthews1986 <alex_matthews@me.com>
2025-05-13 19:38:44 +0100

    Update DialogManager.ts

--- DialogManager.ts
+++ DialogManager.ts
@@ -1,1 +1,29 @@
-// Dialog system logic
+
+import Phaser from 'phaser';
+
+export default class DialogueManager {
+  private scene: Phaser.Scene;
+  private dialogBox!: Phaser.GameObjects.Rectangle;
+  private dialogText!: Phaser.GameObjects.Text;
+
+  constructor(scene: Phaser.Scene) {
+    this.scene = scene;
+  }
+
+  showDialogue(message: string): void {
+    if (this.dialogBox) this.dialogBox.destroy();
+    if (this.dialogText) this.dialogText.destroy();
+
+    this.dialogBox = this.scene.add.rectangle(400, 500, 700, 100, 0x000000, 0.8).setOrigin(0.5);
+    this.dialogText = this.scene.add.text(120, 470, message, {
+      fontSize: '18px',
+      color: '#ffffff',
+      wordWrap: { width: 560 }
+    });
+  }
+
+  hideDialogue(): void {
+    this.dialogBox?.destroy();
+    this.dialogText?.destroy();
+  }
+}
