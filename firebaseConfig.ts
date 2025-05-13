27d7c3ebe3d64954aebf07de45dbc7215191b50d
alexmatthews1986 <alex_matthews@me.com>
2025-05-13 20:08:54 +0100

    Add files via upload

---
+++ firebaseConfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_PROJECT.firebaseapp.com',
+  projectId: 'YOUR_PROJECT',
+  storageBucket: 'YOUR_PROJECT.appspot.com',
+  messagingSenderId: 'YOUR_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
